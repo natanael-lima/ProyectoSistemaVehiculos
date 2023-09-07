a3fa5fe17a97cda52f8e76bb43a1d33d45276cca
DESKTOP-IC9G6U0\Mateo <matuteloto42@gmail.com>
2023-09-06 23:21:25 -0300

    Creacion de clases principales

---
+++ LPOOII_GRUPO02/ClaseBase/TipoVehiculo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseBase
+{
+    public class TipoVehiculo
+    {
+        private int tv_Id;
+
+        public int Tv_Id
+        {
+            get { return tv_Id; }
+            set { tv_Id = value; }
+        }
+        private string tv_Descripcion;
+
+        public string Tv_Descripcion
+        {
+            get { return tv_Descripcion; }
+            set { tv_Descripcion = value; }
+        }
+        private decimal tv_Tarifa;
+
+        public decimal Tv_Tarifa
+        {
+            get { return tv_Tarifa; }
+            set { tv_Tarifa = value; }
+        }
+
+        public TipoVehiculo()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ LPOOII_GRUPO02/ClaseBase/Usuario.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseBase
+{
+    public class Usuario
+    {
+        private int user_Id;
+
+        public int User_Id
+        {
+            get { return user_Id; }
+            set { user_Id = value; }
+        }
+        private string user_Name;
+
+        public string User_Name
+        {
+            get { return user_Name; }
+            set { user_Name = value; }
+        }
+        private string user_Password;
+
+        public string User_Password
+        {
+            get { return user_Password; }
+            set { user_Password = value; }
+        }
+        private string user_Nombre;
+
+        public string User_Nombre
+        {
+            get { return user_Nombre; }
+            set { user_Nombre = value; }
+        }
+        private string user_Apellido;
+
+        public string User_Apellido
+        {
+            get { return user_Apellido; }
+            set { user_Apellido = value; }
+        }
+        private string user_Rol;
+
+        public string User_Rol
+        {
+            get { return user_Rol; }
+            set { user_Rol = value; }
+        }
+
+        public Usuario()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
