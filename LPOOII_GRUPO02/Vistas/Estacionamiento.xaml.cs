d3de61f63281d244644d075f7e93f2abb25a1a38
MarianellaMachicado <machicadomarianella@gmail.com>
2023-09-09 20:05:13 -0300

    Diseño de la ventana vehiculos en playa

---
+++ LPOOII_GRUPO02/Vistas/Estacionamiento.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for Estacionamiento.xaml
+    /// </summary>
+    public partial class Estacionamiento : Window
+    {
+        public Estacionamiento()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            salir();
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            salir();
+        }
+
+       private void salir()
+        {
+            FormMain menuPrincipal = new FormMain();
+            menuPrincipal.Show();
+            this.Close();
+        }
+
+    }
+}
