4ed8e7b595b7d266dbedc0cae9dbf2e59183d872
MarianellaMachicado <machicadomarianella@gmail.com>
2023-09-14 13:30:38 -0300

    Registro de entradas y salidas

--- LPOOII_GRUPO02/Vistas/Estacionamiento.xaml.cs
+++ LPOOII_GRUPO02/Vistas/Estacionamiento.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
 namespace Vistas
 {
     /// <summary>
@@ -18,6 +17,10 @@ namespace Vistas
     /// </summary>
     public partial class Estacionamiento : Window
     {
+        private Brush[] colores = { Brushes.Red,Brushes.Green,Brushes.Gray};
+       
+        private bool sectorDisponible = true;
+
         public Estacionamiento()
         {
             InitializeComponent();
@@ -45,5 +48,44 @@ namespace Vistas
             this.Close();
         }
 
+       private void btnE1_Click(object sender, RoutedEventArgs e)
+       {
+           
+           Button boton = (Button)sender;
+           SolidColorBrush color = boton.Background as SolidColorBrush;
+          
+           if(color != null && color.Color == Colors.Green)
+           {
+               sectorDisponible = true;
+               actualizarEstadoSector(boton);
+           }
+           else
+           {
+               sectorDisponible = false;
+               actualizarEstadoSector(boton);
+           }
+           
+       }
+
+        private void actualizarEstadoSector(Button boton)
+       {
+           if(sectorDisponible)
+           {
+
+               MessageBoxResult resp = MessageBox.Show("Registrar Entrada", "Sector Disponible", MessageBoxButton.YesNo);
+               if (resp == MessageBoxResult.Yes)
+               {
+                   boton.Background = colores[0];//rojo
+               }
+           }
+           else
+           {
+               MessageBoxResult resp = MessageBox.Show("Registrar Salida", "Sector Ocupado", MessageBoxButton.YesNo);
+               if (resp == MessageBoxResult.Yes)
+               {
+                   boton.Background = colores[1];//verde
+               } 
+           }
+       }
     }
 }
